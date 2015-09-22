6226cc89d8b40445601d7b089d940e23d0cd67d7
Rhett Sutphin <rhett@detailedbalance.net>
2015-09-22 11:27:28 -0700

    Enough to do vagrant up && cap deploy:setup.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "hello"
+set :user, 'vagrant'
+set :ssh_options, { keys: ['.vagrant/machines/default/virtualbox/private_key'] }
+set :deploy_to, '/home/vagrant/apps/hello'
+set :use_sudo, false
+
+# TODO
+# set :repository,  "set your repository location here"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+VAGRANT_SERVER="192.168.36.36"
+role :web, VAGRANT_SERVER                          # Your HTTP server, Apache/etc
+role :app, VAGRANT_SERVER                          # This may be the same as your `Web` server
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
